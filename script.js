90f0e023d6dc49fd55ea47caadcfe7345339d3d3
marwaneltary <marwaneltary@gmail.com>
2024-09-15 18:58:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor(){
+     document.getElementById("text").style.color = "#FF2E63";
+}
+
+function changeBGColor(){
+     document.getElementById("color").style.backgroundColor = "#FF2E63";
+}
+
+function changeBG(){
+     document.body.style.backgroundColor =  "#08D9D6";
+}      
